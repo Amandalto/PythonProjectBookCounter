57fd6960bb0fa2552b857de3e8a0d56be12be084
Amanda <amandaltoliveira@gmail.com>
2025-06-17 13:57:46 -0300

    Building a window

---
+++ contador_livros.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+# Criando a janela principal
+app = tk.Tk()
+app.title("Meu Primeiro Tkinter")
+app.geometry("400x300")  # Largura x Altura
+
+# Executando a interface
+app.mainloop()
