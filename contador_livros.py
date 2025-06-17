d61dbf1f346a5e7a13979011f334bbc14a3de931
Amanda <amandaltoliveira@gmail.com>
2025-06-17 14:15:23 -0300

    Adding Text Label and Buttons

--- contador_livros.py
+++ contador_livros.py
@@ -2,8 +2,20 @@ import tkinter as tk
 
 # Criando a janela principal
 app = tk.Tk()
-app.title("Meu Primeiro Tkinter")
-app.geometry("400x300")  # Largura x Altura
+app.title("Contador de Livros") #Título
+app.geometry("800x600")  # Largura x Altura
+
+
+
+#Label Título
+rotulo = tk.Label(app, text = "Bem-Vindo(a) ao Memórias de um Leitor", font=("Arial",14, "bold"), fg="white", bg="purple")
+rotulo.pack(pady=20)
+
+#Campo de entrada
+entrada = tk.Entry(app, width=30)
+entrada.pack(pady=5)
+
+#Botão
 
 # Executando a interface
 app.mainloop()
